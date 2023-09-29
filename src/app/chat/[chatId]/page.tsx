598bd7961a220b20cae0697bf22090349455fe00
Juan <saraviamartin16@hotmail.com>
2023-09-29 15:34:13 -0300

    ChatSidBar

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -1,4 +1,5 @@
 import ChatSideBar from '@/components/ChatSideBar'
+import PDFViewer from '@/components/PDFViewer'
 import { db } from '@/lib/db'
 import { chats } from '@/lib/db/schema'
 import { auth } from '@clerk/nextjs'
@@ -24,16 +25,20 @@ const ChatPage = async ({params: {chatId} }: Props) => {
     // if(!_chats.find(chat => chat.id === parseInt(chatId))){
     //     return redirect('/')
     // }
+
+    const currentChat = _chats.find((chat) => chat.id === parseInt(chatId));
+  const isPro = await checkSubscriptio();
+
     return (
         <div className="flex max-h-screen overflow-scroll">
       <div className="flex w-full max-h-screen overflow-scroll">
         {/* chat sidebar */}
         <div className="flex-[1] max-w-xs">
-          <ChatSideBar chats={_chats} chatId={parseInt(chatId)}  />
+          <ChatSideBar chats={_chats} chatId={parseInt(chatId)} isPro={false}  />
         </div>
         {/* pdf viewer */}
-        {/* <div className="max-h-screen p-4 oveflow-scroll flex-[5]">
-          <PDFViewer pdf_url={currentChat?.pdfUrl || ""} /> */}
+        <div className="max-h-screen p-4 oveflow-scroll flex-[5]">
+          <PDFViewer pdf_url={currentChat?.pdfUrl || ""} />
         </div>
         {/* chat component */}
         {/* <div className="flex-[3] border-l-4 border-l-slate-200">
