9e05f54a1bafdc233cf87c6be761c7b8488290f8
Juan <saraviamartin16@hotmail.com>
2023-10-01 14:19:59 -0300

    subscription, stripe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,15 @@ import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 import {LogIn} from "lucide-react"
 import FileUpload from "@/components/FileUpload";
+import SubscriptionButton from "@/components/SubscriptionButton";
+import { checkSubscription } from "@/lib/subscription";
 
 
 export default async function Home() {
   const {userId} =  await auth()
   const isAuth = !!userId
+  const isPro = await checkSubscription();
+
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" >
@@ -21,6 +25,9 @@ export default async function Home() {
           <div className="flex mt-2">
           {isAuth && (<Button>Go to chat</Button>
               )}
+              <div className="ml-3">
+                  <SubscriptionButton isPro={isPro} />
+                </div>
           </div>
 
         <p className="max-w-xl mt-1 text-lg text-slate-600">
